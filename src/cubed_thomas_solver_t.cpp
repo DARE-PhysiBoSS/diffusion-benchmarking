7315f7df50b95f5129225375245ebafc2673904a
Adam Smelko <adamsmelko1@gmail.com>
2025-05-08 10:13:36 +0000

    cubed fixes

--- src/cubed_thomas_solver_t.cpp
+++ src/cubed_thomas_solver_t.cpp
@@ -66,6 +66,8 @@ void cubed_thomas_solver_t<real_t, aligned_x>::tune(const nlohmann::json& params
 	block_size_ = params.contains("block_size") ? (std::size_t)params["block_size"] : 100;
 	alignment_size_ = params.contains("alignment_size") ? (std::size_t)params["alignment_size"] : 64;
 	x_tile_size_ = params.contains("x_tile_size") ? (std::size_t)params["x_tile_size"] : 48;
+	cores_division_ = params.contains("cores_division") ? (std::array<index_t, 3>)params["cores_division"]
+														: std::array<index_t, 3> { 2, 2, 2 };
 }
 
 template <typename real_t, bool aligned_x>
@@ -81,8 +83,8 @@ void cubed_thomas_solver_t<real_t, aligned_x>::initialize()
 		precompute_values(az_, b1z_, this->problem_.dz, this->problem_.dims, this->problem_.nz, countersz_count_,
 						  countersz_, max_threadsz_);
 
-	// cores_division_ = { 2, 4, 4 };
-	cores_division_ = { 4, 2 };
+	if (this->problem_.dims == 2)
+		cores_division_[2] = 1;
 
 	auto scratch_layout = get_scratch_layout();
 
@@ -496,7 +498,8 @@ static void solve_block_y_end(real_t* __restrict__ densities, real_t* __restrict
 template <typename index_t, typename real_t, typename density_layout_t, typename scratch_layout_t>
 static void solve_block_z_start(real_t* __restrict__ densities, const real_t* __restrict__ ac,
 								const real_t* __restrict__ b1, real_t* __restrict__ a_data, real_t* __restrict__ c_data,
-								const density_layout_t dens_l, const scratch_layout_t scratch_l)
+								const density_layout_t dens_l, const scratch_layout_t scratch_l, const bool begin,
+								const bool end)
 {
 	constexpr char dim = 'z';
 	const index_t s_len = dens_l | noarr::get_length<'s'>();
@@ -515,9 +518,9 @@ static void solve_block_z_start(real_t* __restrict__ densities, const real_t* __
 		{
 			const auto state = noarr::idx<dim>(i);
 
-			const auto a_tmp = ac[s] * (i == 0 ? 0 : 1);
-			const auto b_tmp = b1[s] + (i == 0 || i == n - 1 ? ac[s] : 0);
-			const auto c_tmp = ac[s] * (i == n - 1 ? 0 : 1);
+			const auto a_tmp = ac[s] * (begin && i == 0 ? 0 : 1);
+			const auto b_tmp = b1[s] + ((begin && i == 0) || (end && i == n - 1) ? ac[s] : 0);
+			const auto c_tmp = ac[s] * (end && i == n - 1 ? 0 : 1);
 
 			a[state] = a_tmp / b_tmp;
 			c[state] = c_tmp / b_tmp;
@@ -539,9 +542,9 @@ static void solve_block_z_start(real_t* __restrict__ densities, const real_t* __
 			const auto state = noarr::idx<dim>(i);
 			const auto prev_state = noarr::idx<dim>(i - 1);
 
-			const auto a_tmp = ac[s] * (i == 0 ? 0 : 1);
-			const auto b_tmp = b1[s] + (i == n - 1 ? ac[s] : 0);
-			const auto c_tmp = ac[s] * (i == n - 1 ? 0 : 1);
+			const auto a_tmp = ac[s] * (begin && i == 0 ? 0 : 1);
+			const auto b_tmp = b1[s] + (end && i == n - 1 ? ac[s] : 0);
+			const auto c_tmp = ac[s] * (end && i == n - 1 ? 0 : 1);
 
 			const auto r = 1 / (b_tmp - a_tmp * c[prev_state]);
 
@@ -617,8 +620,8 @@ static void solve_block_z_middle(real_t* __restrict__ densities, real_t* __restr
 
 	for (index_t s = 0; s < s_len; s++)
 	{
-		auto a = noarr::make_bag(scratch_l ^ noarr::fix<'s'>(s), a_data);
-		auto c = noarr::make_bag(scratch_l ^ noarr::fix<'s'>(s), c_data);
+		auto a = noarr::make_bag(scratch_l ^ noarr::rename<'i', dim>() ^ noarr::fix<'s'>(s), a_data);
+		auto c = noarr::make_bag(scratch_l ^ noarr::rename<'i', dim>() ^ noarr::fix<'s'>(s), c_data);
 
 		auto get_i = [block_size, n](index_t equation_idx) {
 			const index_t block_idx = equation_idx / 2;
@@ -676,8 +679,8 @@ static void solve_block_z_end(real_t* __restrict__ densities, real_t* __restrict
 
 	for (index_t s = 0; s < s_len; s++)
 	{
-		auto a = noarr::make_bag(scratch_l ^ noarr::fix<'s'>(s), a_data);
-		auto c = noarr::make_bag(scratch_l ^ noarr::fix<'s'>(s), c_data);
+		auto a = noarr::make_bag(scratch_l ^ noarr::rename<'i', dim>() ^ noarr::fix<'s'>(s), a_data);
+		auto c = noarr::make_bag(scratch_l ^ noarr::rename<'i', dim>() ^ noarr::fix<'s'>(s), c_data);
 
 		// Final part of modified thomas algorithm
 		// Solve the rest of the unknowns
@@ -703,23 +706,28 @@ static void solve_block_z_end(real_t* __restrict__ densities, real_t* __restrict
 }
 
 template <typename index_t, typename real_t, typename density_layout_t, typename scratch_layout_t>
-static void solve_2d(real_t* __restrict__ densities, const density_layout_t dens_l, const real_t* __restrict__ acx,
-					 const real_t* __restrict__ b1x, const real_t* __restrict__ acy, const real_t* __restrict__ b1y,
-					 real_t* __restrict__ a_data, real_t* __restrict__ c_data, const scratch_layout_t scratch_l,
-					 std::array<index_t, 3> cores_division)
+static void solve_2d_and_3d(real_t* __restrict__ densities, const density_layout_t dens_l,
+							const real_t* __restrict__ acx, const real_t* __restrict__ b1x,
+							const real_t* __restrict__ acy, const real_t* __restrict__ b1y,
+							const real_t* __restrict__ acz, const real_t* __restrict__ b1z, real_t* __restrict__ a_data,
+							real_t* __restrict__ c_data, const scratch_layout_t scratch_l,
+							std::array<index_t, 3> cores_division)
 {
 	const index_t x_len = dens_l | noarr::get_length<'x'>();
 	const index_t y_len = dens_l | noarr::get_length<'y'>();
+	const index_t z_len = dens_l | noarr::get_length<'z'>();
 
 	const index_t block_size_x = (x_len + cores_division[0] - 1) / cores_division[0];
 	const index_t block_size_y = (y_len + cores_division[1] - 1) / cores_division[1];
+	const index_t block_size_z = (z_len + cores_division[2] - 1) / cores_division[2];
 
-#pragma omp parallel num_threads(cores_division[0] * cores_division[1])
+#pragma omp parallel num_threads(cores_division[0] * cores_division[1] * cores_division[2])
 	{
 		const auto tid = get_thread_num();
 
 		const auto tid_x = tid % cores_division[0];
-		const auto tid_y = tid / cores_division[0];
+		const auto tid_y = (tid / cores_division[0]) % cores_division[1];
+		const auto tid_z = tid / (cores_division[0] * cores_division[1]);
 
 		const auto block_x_begin = tid_x * block_size_x;
 		const auto block_x_end = std::min(block_x_begin + block_size_x, x_len);
@@ -729,15 +737,21 @@ static void solve_2d(real_t* __restrict__ densities, const density_layout_t dens
 		const auto block_y_end = std::min(block_y_begin + block_size_y, y_len);
 		const auto block_y_len = block_y_end - block_y_begin;
 
-		const auto thread_dens_l =
-			dens_l ^ noarr::slice<'x'>(block_x_begin, block_x_len) ^ noarr::slice<'y'>(block_y_begin, block_y_len);
+		const auto block_z_begin = tid_z * block_size_z;
+		const auto block_z_end = std::min(block_z_begin + block_size_z, z_len);
+		const auto block_z_len = block_z_end - block_z_begin;
 
-		// Do X
+		const auto thread_dens_l = dens_l ^ noarr::slice<'x'>(block_x_begin, block_x_len)
+								   ^ noarr::slice<'y'>(block_y_begin, block_y_len)
+								   ^ noarr::slice<'z'>(block_z_begin, block_z_len);
+
+		// do x
 		{
-			const auto lane_scratch_l = scratch_l ^ noarr::fix<'l'>(tid_y);
+			const auto lane_scratch_l = scratch_l ^ noarr::fix<'l'>(tid_y * cores_division[2] + tid_z);
 			const auto thread_scratch_l = lane_scratch_l ^ noarr::slice<'i'>(block_x_begin, block_x_len);
 
-			const auto lane_dens_l = dens_l ^ noarr::slice<'y'>(block_y_begin, block_y_len);
+			const auto lane_dens_l =
+				dens_l ^ noarr::slice<'y'>(block_y_begin, block_y_len) ^ noarr::slice<'z'>(block_z_begin, block_z_len);
 
 			solve_block_x_start<index_t>(densities, acx, b1x, a_data, c_data, thread_dens_l, thread_scratch_l,
 										 tid_x == 0, tid_x == cores_division[0] - 1);
@@ -754,12 +768,13 @@ static void solve_2d(real_t* __restrict__ densities, const density_layout_t dens
 
 #pragma omp barrier
 
-		// Do Y
+		// do Y
 		{
-			const auto lane_scratch_l = scratch_l ^ noarr::fix<'l'>(tid_x);
+			const auto lane_scratch_l = scratch_l ^ noarr::fix<'l'>(tid_x * cores_division[2] + tid_z);
 			const auto thread_scratch_l = lane_scratch_l ^ noarr::slice<'i'>(block_y_begin, block_y_len);
 
-			const auto lane_dens_l = dens_l ^ noarr::slice<'x'>(block_x_begin, block_x_len);
+			const auto lane_dens_l =
+				dens_l ^ noarr::slice<'x'>(block_x_begin, block_x_len) ^ noarr::slice<'z'>(block_z_begin, block_z_len);
 
 			solve_block_y_start<index_t>(densities, acy, b1y, a_data, c_data, thread_dens_l, thread_scratch_l,
 										 tid_y == 0, tid_y == cores_division[1] - 1);
@@ -773,6 +788,29 @@ static void solve_2d(real_t* __restrict__ densities, const density_layout_t dens
 
 			solve_block_y_end<index_t>(densities, a_data, c_data, thread_dens_l, thread_scratch_l);
 		}
+
+		if (z_len > 1)
+		{
+#pragma omp barrier
+
+			// do Z
+			{
+				const auto lane_scratch_l = scratch_l ^ noarr::fix<'l'>(tid_x * cores_division[1] + tid_y);
+				const auto thread_scratch_l = lane_scratch_l ^ noarr::slice<'i'>(block_z_begin, block_z_len);
+
+				const auto lane_dens_l = dens_l ^ noarr::slice<'x'>(block_x_begin, block_x_len)
+										 ^ noarr::slice<'y'>(block_y_begin, block_y_len);
+
+				solve_block_z_start<index_t>(densities, acz, b1z, a_data, c_data, thread_dens_l, thread_scratch_l,
+											 tid_z == 0, tid_z == cores_division[2] - 1);
+#pragma omp barrier
+				if (tid_z == 0)
+					solve_block_z_middle(densities, a_data, c_data, lane_dens_l, lane_scratch_l, block_size_z);
+#pragma omp barrier
+
+				solve_block_z_end<index_t>(densities, a_data, c_data, thread_dens_l, thread_scratch_l);
+			}
+		}
 	}
 }
 
@@ -784,16 +822,19 @@ static void solve_slice_x_2d_and_3d(real_t* __restrict__ densities, const densit
 {
 	const index_t x_len = dens_l | noarr::get_length<'x'>();
 	const index_t y_len = dens_l | noarr::get_length<'y'>();
+	const index_t z_len = dens_l | noarr::get_length<'z'>();
 
 	const index_t block_size_x = (x_len + cores_division[0] - 1) / cores_division[0];
 	const index_t block_size_y = (y_len + cores_division[1] - 1) / cores_division[1];
+	const index_t block_size_z = (z_len + cores_division[2] - 1) / cores_division[2];
 
-#pragma omp parallel num_threads(cores_division[0] * cores_division[1])
+#pragma omp parallel num_threads(cores_division[0] * cores_division[1] * cores_division[2])
 	{
 		const auto tid = get_thread_num();
 
 		const auto tid_x = tid % cores_division[0];
-		const auto tid_y = tid / cores_division[0];
+		const auto tid_y = (tid / cores_division[0]) % cores_division[1];
+		const auto tid_z = tid / (cores_division[0] * cores_division[1]);
 
 		const auto block_x_begin = tid_x * block_size_x;
 		const auto block_x_end = std::min(block_x_begin + block_size_x, x_len);
@@ -803,15 +844,21 @@ static void solve_slice_x_2d_and_3d(real_t* __restrict__ densities, const densit
 		const auto block_y_end = std::min(block_y_begin + block_size_y, y_len);
 		const auto block_y_len = block_y_end - block_y_begin;
 
-		const auto thread_dens_l =
-			dens_l ^ noarr::slice<'x'>(block_x_begin, block_x_len) ^ noarr::slice<'y'>(block_y_begin, block_y_len);
+		const auto block_z_begin = tid_z * block_size_z;
+		const auto block_z_end = std::min(block_z_begin + block_size_z, z_len);
+		const auto block_z_len = block_z_end - block_z_begin;
 
-		// Do X
+		const auto thread_dens_l = dens_l ^ noarr::slice<'x'>(block_x_begin, block_x_len)
+								   ^ noarr::slice<'y'>(block_y_begin, block_y_len)
+								   ^ noarr::slice<'z'>(block_z_begin, block_z_len);
+
+		// do x
 		{
-			const auto lane_scratch_l = scratch_l ^ noarr::fix<'l'>(tid_y);
+			const auto lane_scratch_l = scratch_l ^ noarr::fix<'l'>(tid_y * cores_division[2] + tid_z);
 			const auto thread_scratch_l = lane_scratch_l ^ noarr::slice<'i'>(block_x_begin, block_x_len);
 
-			const auto lane_dens_l = dens_l ^ noarr::slice<'y'>(block_y_begin, block_y_len);
+			const auto lane_dens_l =
+				dens_l ^ noarr::slice<'y'>(block_y_begin, block_y_len) ^ noarr::slice<'z'>(block_z_begin, block_z_len);
 
 			solve_block_x_start<index_t>(densities, ac, b1, a_data, c_data, thread_dens_l, thread_scratch_l, tid_x == 0,
 										 tid_x == cores_division[0] - 1);
@@ -836,16 +883,19 @@ static void solve_slice_y_2d_and_3d(real_t* __restrict__ densities, const densit
 {
 	const index_t x_len = dens_l | noarr::get_length<'x'>();
 	const index_t y_len = dens_l | noarr::get_length<'y'>();
+	const index_t z_len = dens_l | noarr::get_length<'z'>();
 
 	const index_t block_size_x = (x_len + cores_division[0] - 1) / cores_division[0];
 	const index_t block_size_y = (y_len + cores_division[1] - 1) / cores_division[1];
+	const index_t block_size_z = (z_len + cores_division[2] - 1) / cores_division[2];
 
-#pragma omp parallel num_threads(cores_division[0] * cores_division[1])
+#pragma omp parallel num_threads(cores_division[0] * cores_division[1] * cores_division[2])
 	{
 		const auto tid = get_thread_num();
 
 		const auto tid_x = tid % cores_division[0];
-		const auto tid_y = tid / cores_division[0];
+		const auto tid_y = (tid / cores_division[0]) % cores_division[1];
+		const auto tid_z = tid / (cores_division[0] * cores_division[1]);
 
 		const auto block_x_begin = tid_x * block_size_x;
 		const auto block_x_end = std::min(block_x_begin + block_size_x, x_len);
@@ -855,15 +905,21 @@ static void solve_slice_y_2d_and_3d(real_t* __restrict__ densities, const densit
 		const auto block_y_end = std::min(block_y_begin + block_size_y, y_len);
 		const auto block_y_len = block_y_end - block_y_begin;
 
-		const auto thread_dens_l =
-			dens_l ^ noarr::slice<'x'>(block_x_begin, block_x_len) ^ noarr::slice<'y'>(block_y_begin, block_y_len);
+		const auto block_z_begin = tid_z * block_size_z;
+		const auto block_z_end = std::min(block_z_begin + block_size_z, z_len);
+		const auto block_z_len = block_z_end - block_z_begin;
 
-		// Do Y
+		const auto thread_dens_l = dens_l ^ noarr::slice<'x'>(block_x_begin, block_x_len)
+								   ^ noarr::slice<'y'>(block_y_begin, block_y_len)
+								   ^ noarr::slice<'z'>(block_z_begin, block_z_len);
+
+		// do Y
 		{
-			const auto lane_scratch_l = scratch_l ^ noarr::fix<'l'>(tid_x);
+			const auto lane_scratch_l = scratch_l ^ noarr::fix<'l'>(tid_x * cores_division[2] + tid_z);
 			const auto thread_scratch_l = lane_scratch_l ^ noarr::slice<'i'>(block_y_begin, block_y_len);
 
-			const auto lane_dens_l = dens_l ^ noarr::slice<'x'>(block_x_begin, block_x_len);
+			const auto lane_dens_l =
+				dens_l ^ noarr::slice<'x'>(block_x_begin, block_x_len) ^ noarr::slice<'z'>(block_z_begin, block_z_len);
 
 			solve_block_y_start<index_t>(densities, ac, b1, a_data, c_data, thread_dens_l, thread_scratch_l, tid_y == 0,
 										 tid_y == cores_division[1] - 1);
@@ -876,6 +932,64 @@ static void solve_slice_y_2d_and_3d(real_t* __restrict__ densities, const densit
 #pragma omp barrier
 
 			solve_block_y_end<index_t>(densities, a_data, c_data, thread_dens_l, thread_scratch_l);
+		}
+	}
+}
+
+template <typename index_t, typename real_t, typename density_layout_t, typename scratch_layout_t>
+static void solve_slice_z_2d_and_3d(real_t* __restrict__ densities, const density_layout_t dens_l,
+									const real_t* __restrict__ ac, const real_t* __restrict__ b1,
+									real_t* __restrict__ a_data, real_t* __restrict__ c_data,
+									const scratch_layout_t scratch_l, std::array<index_t, 3> cores_division)
+{
+	const index_t x_len = dens_l | noarr::get_length<'x'>();
+	const index_t y_len = dens_l | noarr::get_length<'y'>();
+	const index_t z_len = dens_l | noarr::get_length<'z'>();
+
+	const index_t block_size_x = (x_len + cores_division[0] - 1) / cores_division[0];
+	const index_t block_size_y = (y_len + cores_division[1] - 1) / cores_division[1];
+	const index_t block_size_z = (z_len + cores_division[2] - 1) / cores_division[2];
+
+#pragma omp parallel num_threads(cores_division[0] * cores_division[1] * cores_division[2])
+	{
+		const auto tid = get_thread_num();
+
+		const auto tid_x = tid % cores_division[0];
+		const auto tid_y = (tid / cores_division[0]) % cores_division[1];
+		const auto tid_z = tid / (cores_division[0] * cores_division[1]);
+
+		const auto block_x_begin = tid_x * block_size_x;
+		const auto block_x_end = std::min(block_x_begin + block_size_x, x_len);
+		const auto block_x_len = block_x_end - block_x_begin;
+
+		const auto block_y_begin = tid_y * block_size_y;
+		const auto block_y_end = std::min(block_y_begin + block_size_y, y_len);
+		const auto block_y_len = block_y_end - block_y_begin;
+
+		const auto block_z_begin = tid_z * block_size_z;
+		const auto block_z_end = std::min(block_z_begin + block_size_z, z_len);
+		const auto block_z_len = block_z_end - block_z_begin;
+
+		const auto thread_dens_l = dens_l ^ noarr::slice<'x'>(block_x_begin, block_x_len)
+								   ^ noarr::slice<'y'>(block_y_begin, block_y_len)
+								   ^ noarr::slice<'z'>(block_z_begin, block_z_len);
+
+		// do Z
+		{
+			const auto lane_scratch_l = scratch_l ^ noarr::fix<'l'>(tid_x * cores_division[1] + tid_y);
+			const auto thread_scratch_l = lane_scratch_l ^ noarr::slice<'i'>(block_z_begin, block_z_len);
+
+			const auto lane_dens_l =
+				dens_l ^ noarr::slice<'x'>(block_x_begin, block_x_len) ^ noarr::slice<'y'>(block_y_begin, block_y_len);
+
+			solve_block_z_start<index_t>(densities, ac, b1, a_data, c_data, thread_dens_l, thread_scratch_l, tid_z == 0,
+										 tid_z == cores_division[2] - 1);
+#pragma omp barrier
+			if (tid_z == 0)
+				solve_block_z_middle(densities, a_data, c_data, lane_dens_l, lane_scratch_l, block_size_z);
+#pragma omp barrier
+
+			solve_block_z_end<index_t>(densities, a_data, c_data, thread_dens_l, thread_scratch_l);
 		}
 	}
 }
@@ -903,7 +1017,10 @@ void cubed_thomas_solver_t<real_t, aligned_x>::solve_y()
 
 template <typename real_t, bool aligned_x>
 void cubed_thomas_solver_t<real_t, aligned_x>::solve_z()
-{}
+{
+	solve_slice_z_2d_and_3d<index_t>(this->substrates_, get_substrates_layout<3>(), az_, b1z_, a_scratch_, c_scratch_,
+									 get_scratch_layout(), cores_division_);
+}
 
 template <typename real_t, bool aligned_x>
 void cubed_thomas_solver_t<real_t, aligned_x>::solve()
@@ -912,12 +1029,11 @@ void cubed_thomas_solver_t<real_t, aligned_x>::solve()
 	{
 		return;
 	}
-	else if (this->problem_.dims == 2)
+	else
 	{
-		solve_2d(this->substrates_, get_substrates_layout<3>(), ax_, b1x_, ay_, b1y_, a_scratch_, c_scratch_,
-				 get_scratch_layout(), cores_division_);
+		solve_2d_and_3d(this->substrates_, get_substrates_layout<3>(), ax_, b1x_, ay_, b1y_, az_, b1z_, a_scratch_,
+						c_scratch_, get_scratch_layout(), cores_division_);
 	}
-	else {}
 }
 
 template <typename real_t, bool aligned_x>
