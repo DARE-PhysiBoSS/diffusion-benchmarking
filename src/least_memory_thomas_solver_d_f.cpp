c25bff867158e619ac4a71501bc8cf8e7f822351
Adam Smelko <adamsmelko1@gmail.com>
2025-06-05 06:58:40 +0000

    some fixes

--- src/least_memory_thomas_solver_d_f.cpp
+++ src/least_memory_thomas_solver_d_f.cpp
@@ -2010,7 +2010,7 @@ struct inner_y_data_blocked_alt
 	scratch_bag_t cy_scratch;
 };
 
-template <bool z_blocked, typename vec_t, typename index_t, typename real_t, typename simd_tag,
+template <bool z_blocked, bool alt_blocked, typename vec_t, typename index_t, typename real_t, typename simd_tag,
 		  typename density_layout_t, typename diag_bag_t, typename z_data_t>
 constexpr static void y_backward_vectorized(const density_layout_t dens_l, real_t* __restrict__ densities,
 											const diag_bag_t c, simd_tag t, const index_t z, const index_t s,
@@ -2036,25 +2036,33 @@ constexpr static void y_backward_vectorized(const density_layout_t dens_l, real_
 			curr = hn::MulAdd(hn::Set(t, -c.template at<'i'>(i)), prev, curr);
 			// hn::Store(curr, t, &d.template at<'z', 'y'>(z, i));
 
-			if constexpr (z_blocked)
+			if constexpr (!z_blocked)
+				z_forward_inside_y_vectorized(d, t, z, i + 1, prev, az_s, b1z_s, z_len, z_data.cz_tmp);
+			else if constexpr (!alt_blocked)
 				z_forward_inside_y_vectorized_blocked(d, t, s, z, i + 1, prev, az_s, b1z_s, z_data.z_begin,
 													  z_data.cz_scratch);
 			else
-				z_forward_inside_y_vectorized(d, t, z, i + 1, prev, az_s, b1z_s, z_len, z_data.cz_tmp);
+				z_forward_inside_y_blocked_alt_vectorized(d, t, s, i + 1, z, X, prev, az_s, b1z_s, z_data.begin,
+														  z_data.end, z_data.a, z_data.c);
 
 			prev = curr;
 		}
 
-		if constexpr (z_blocked)
+		if constexpr (!z_blocked)
+			r_z = z_forward_inside_y_vectorized(d, t, z, 0, prev, az_s, b1z_s, z_len, z_data.cz_tmp);
+		else if constexpr (!alt_blocked)
 			z_forward_inside_y_vectorized_blocked(d, t, s, z, 0, prev, az_s, b1z_s, z_data.z_begin, z_data.cz_scratch);
 		else
-			r_z = z_forward_inside_y_vectorized(d, t, z, 0, prev, az_s, b1z_s, z_len, z_data.cz_tmp);
+			z_forward_inside_y_blocked_alt_vectorized(d, t, s, 0, z, X, prev, az_s, b1z_s, z_data.begin, z_data.end,
+													  z_data.a, z_data.c);
 	}
 
-	if constexpr (z_blocked)
+	if constexpr (!z_blocked)
+		z_data.cz_tmp = az_s * r_z;
+	else if constexpr (!alt_blocked)
 		z_forward_inside_y_blocked_next(s, z, az_s, b1z_s, z_len, z_data.z_begin, z_data.az_scratch, z_data.cz_scratch);
 	else
-		z_data.cz_tmp = az_s * r_z;
+		z_forward_inside_y_blocked_next_alt(s, z, az_s, b1z_s, z_len, z_data.begin, z_data.end, z_data.a, z_data.c);
 }
 
 template <typename vec_t, typename index_t, typename real_t, typename simd_tag, typename density_layout_t,
@@ -2330,8 +2338,8 @@ static void solve_slice_xyz_fused_transpose(real_t* __restrict__ densities, cons
 				}
 			}
 
-			y_backward_vectorized<false, simd_t>(dens_l, densities, cy, t, z, s, simd_length, y_len, az_s, b1z_s,
-												 inner_z_data { cz_tmp });
+			y_backward_vectorized<false, false, simd_t>(dens_l, densities, cy, t, z, s, simd_length, y_len, az_s, b1z_s,
+														inner_z_data { cz_tmp });
 		}
 
 		z_backward<simd_t>(dens_l, densities, cz, t, s, simd_length, y_len, z_len);
@@ -2478,8 +2486,8 @@ static void solve_slice_xyz_fused_transpose_blocked(real_t* __restrict__ densiti
 				}
 			}
 
-			y_backward_vectorized<true, simd_t>(dens_l, densities, cy, t, z, s, simd_length, y_len, az_s, b1z_s,
-												inner_z_data_blocked { z_begin, a_scratch, c_scratch });
+			y_backward_vectorized<true, false, simd_t>(dens_l, densities, cy, t, z, s, simd_length, y_len, az_s, b1z_s,
+													   inner_z_data_blocked { z_begin, a_scratch, c_scratch });
 		}
 
 		z_backward_blocked(d, 0, y_len, z_begin, z_end, a_scratch, c_scratch, s);
@@ -2516,6 +2524,116 @@ static void solve_slice_xyz_fused_transpose_blocked(real_t* __restrict__ densiti
 		}
 
 		z_blocked_end(d, a_scratch, c_scratch, 0, y_len, z_begin, z_end, s);
+	}
+}
+
+
+template <typename index_t, typename real_t, typename density_layout_t, typename diagonal_layout_t,
+		  typename scratch_layout_t>
+static void solve_slice_xyz_fused_transpose_blocked_alt(
+	real_t* __restrict__ densities, const real_t* __restrict__ ax, const real_t* __restrict__ b1x,
+	const real_t* __restrict__ back_cx, const real_t* __restrict__ ay, const real_t* __restrict__ b1y,
+	const real_t* __restrict__ back_cy, const real_t* __restrict__ az, const real_t* __restrict__ b1z,
+	real_t* __restrict__ a_data, real_t* __restrict__ c_data, const density_layout_t dens_l,
+	const diagonal_layout_t diagx_l, const diagonal_layout_t diagy_l, const scratch_layout_t scratch_l,
+	const index_t s_begin, const index_t s_end, const index_t z_begin, const index_t z_end, const index_t coop_size,
+	index_t& epoch, std::atomic<long>& counter)
+{
+	using simd_tag = hn::ScalableTag<real_t>;
+	simd_tag t;
+	HWY_LANES_CONSTEXPR index_t simd_length = hn::Lanes(t);
+	using simd_t = hn::Vec<simd_tag>;
+
+	const auto d = noarr::make_bag(dens_l, densities);
+
+	const index_t y_len = dens_l | noarr::get_length<'y'>();
+
+	const auto a_scratch = noarr::make_bag(scratch_l, a_data);
+	const auto c_scratch = noarr::make_bag(scratch_l, c_data);
+
+	auto blocked_dens_l = dens_l ^ noarr::into_blocks_static<'y', 'b', 'y', 'v'>(simd_length);
+
+	for (index_t s = s_begin; s < s_end; s++)
+	{
+		const real_t ax_s = ax[s];
+		const real_t b1x_s = b1x[s];
+
+		const real_t ay_s = ay[s];
+		const real_t b1y_s = b1y[s];
+
+		const real_t az_s = az[s];
+		const real_t b1z_s = b1z[s];
+
+		auto cx = noarr::make_bag(diagx_l ^ noarr::fix<'s'>(s), back_cx);
+		auto cy = noarr::make_bag(diagy_l ^ noarr::fix<'s'>(s), back_cy);
+
+		for (index_t z = z_begin; z < z_end; z++)
+		{
+			real_t cy_tmp = ay_s;
+
+			{
+				auto body_dens_l = blocked_dens_l ^ noarr::fix<'b'>(noarr::lit<0>);
+				const index_t y_len_body = (body_dens_l | noarr::get_length<'y'>()) * simd_length;
+				const auto d = noarr::make_bag(dens_l, densities);
+
+				xy_fused_transpose_part<false, false, simd_t>(d, t, simd_length, 0, y_len_body, z, s, ax_s, b1x_s, ay_s,
+															  b1y_s, cx, inside_data { cy_tmp });
+			}
+
+			// y remainder
+			{
+				auto rem_dens_l = blocked_dens_l ^ noarr::fix<'b'>(noarr::lit<1>);
+				const index_t y_len_remainder = rem_dens_l | noarr::get_length<'v'>();
+
+				for (index_t y = y_len - y_len_remainder; y < y_len; y++)
+				{
+					real_t a_tmp = 0;
+					real_t b_tmp = b1x_s + ax_s;
+					real_t c_tmp = ax_s;
+					real_t prev = 0;
+
+					x_forward(d, s, z, y, ax_s, b1x_s, a_tmp, b_tmp, c_tmp, prev);
+
+					x_backward<false, false>(d, cy, s, z, y, prev, ay_s, b1y_s, inside_data { cy_tmp });
+
+					y_forward_inside_x<true>(d, s, z, y, 0, prev, ay_s, b1y_s, cy_tmp);
+				}
+			}
+
+			y_backward_vectorized<true, true, simd_t>(dens_l, densities, cy, t, z, s, simd_length, y_len, az_s, b1z_s,
+													  inside_data_blocked_alt { z_begin, z_end, a_scratch, c_scratch });
+		}
+
+		{
+			epoch++;
+
+			auto val = counter.fetch_add(1, std::memory_order_acq_rel) + 1;
+
+			while (val < epoch * coop_size)
+			{
+				val = counter.load(std::memory_order_acquire);
+			}
+		}
+
+		// #pragma omp barrier
+
+		if (z_begin == 0)
+			z_blocked_middle(d, a_scratch, c_scratch, coop_size, 0, y_len, s);
+
+		// #pragma omp barrier
+
+		{
+			epoch++;
+
+			auto val = counter.fetch_add(1, std::memory_order_acq_rel) + 1;
+
+			while (val < epoch * coop_size)
+			{
+				val = counter.load(std::memory_order_acquire);
+			}
+		}
+
+		z_blocked_end_alt(d, a_scratch, c_scratch, 0, y_len, z_begin, z_end, s);
 	}
 }
 
@@ -2848,17 +2966,14 @@ static void solve_slice_xyz_fused_transpose_blocked(
 				}
 			}
 
-			for (index_t x = 0; x < x_len; x++)
-			{
-				z_forward_inside_y_blocked(d, s, z, y_begin, x, d.template at<'s', 'x', 'y', 'z'>(s, x, y_begin, z),
-										   az_s, b1z_s, z_begin, cz_scratch);
-			}
-
 			y_blocked_end(d, z, ay_scratch, cy_scratch, y_begin, y_end, s, az_s, b1z_s,
 						  inside_data_blocked { z_begin, cz_scratch });
 
 			for (index_t x = 0; x < x_len; x++)
 			{
+				z_forward_inside_y_blocked(d, s, z, y_begin, x, d.template at<'s', 'x', 'y', 'z'>(s, x, y_begin, z),
+										   az_s, b1z_s, z_begin, cz_scratch);
+
 				z_forward_inside_y_blocked(d, s, z, y_end - 1, x, d.template at<'s', 'x', 'y', 'z'>(s, x, y_end - 1, z),
 										   az_s, b1z_s, z_begin, cz_scratch);
 			}
@@ -2925,7 +3040,6 @@ static void solve_slice_xyz_fused_transpose_blocked_alt(
 
 	const index_t z_len = dens_l | noarr::get_length<'z'>();
 	const index_t y_len = dens_l | noarr::get_length<'y'>();
-	const index_t x_len = dens_l | noarr::get_length<'x'>();
 
 	const auto ay_scratch = noarr::make_bag(scratchy_l, ay_data);
 	const auto cy_scratch = noarr::make_bag(scratchy_l, cy_data);
@@ -3226,11 +3340,20 @@ void least_memory_thomas_solver_d_f<real_t, aligned_x>::solve_blocked_3d()
 			for (index_t i = 0; i < this->problem_.iterations; i++)
 			{
 				if (cores_division_[1] == 1)
-					solve_slice_xyz_fused_transpose_blocked<index_t>(
-						this->substrates_, ax_, b1x_, cx_, ay_, b1y_, cy_, az_, b1z_, a_scratchz_, c_scratchz_,
-						get_substrates_layout<3>(), get_diagonal_layout(this->problem_, this->problem_.nx),
-						get_diagonal_layout(this->problem_, this->problem_.ny), lane_scratchz_l, s, s + s_step_length,
-						block_z_begin, block_z_end, cores_division_[2], epoch_z, countersz_[lane_id_z].value);
+					if (!use_alt_blocked_)
+						solve_slice_xyz_fused_transpose_blocked<index_t>(
+							this->substrates_, ax_, b1x_, cx_, ay_, b1y_, cy_, az_, b1z_, a_scratchz_, c_scratchz_,
+							get_substrates_layout<3>(), get_diagonal_layout(this->problem_, this->problem_.nx),
+							get_diagonal_layout(this->problem_, this->problem_.ny), lane_scratchz_l, s,
+							s + s_step_length, block_z_begin, block_z_end, cores_division_[2], epoch_z,
+							countersz_[lane_id_z].value);
+					else
+						solve_slice_xyz_fused_transpose_blocked_alt<index_t>(
+							this->substrates_, ax_, b1x_, cx_, ay_, b1y_, cy_, az_, b1z_, a_scratchz_, c_scratchz_,
+							get_substrates_layout<3>(), get_diagonal_layout(this->problem_, this->problem_.nx),
+							get_diagonal_layout(this->problem_, this->problem_.ny), lane_scratchz_l, s,
+							s + s_step_length, block_z_begin, block_z_end, cores_division_[2], epoch_z,
+							countersz_[lane_id_z].value);
 				else
 				{
 					if (!use_alt_blocked_)
