76b853919462da3e15942f1306ac3f387094d843
Adam Smelko <adamsmelko1@gmail.com>
2025-08-15 10:13:07 +0000

    Fix ref

--- src/space-dependent-diffusion/reference_thomas_solver.cpp
+++ src/space-dependent-diffusion/reference_thomas_solver.cpp
@@ -2,7 +2,8 @@
 
 template <typename real_t>
 void sdd_reference_thomas_solver<real_t>::precompute_values(std::unique_ptr<real_t[]>& a, std::unique_ptr<real_t[]>& b,
-															std::unique_ptr<real_t[]>& c, index_t shape, index_t dims)
+															std::unique_ptr<real_t[]>& c, index_t shape, index_t n,
+															index_t dims, char dim)
 {
 	auto substrates_layout = get_substrates_layout();
 
@@ -25,17 +26,38 @@ void sdd_reference_thomas_solver<real_t>::precompute_values(std::unique_ptr<real
 				{
 					auto idx = noarr::idx<'x', 'y', 'z', 's'>(x, y, z, s);
 
-					a_bag[idx] = -this->problem_.dt * get_diffusion_coefficients(x, y, z, s) / (shape * shape);
-					b_bag[idx] = 1 + this->problem_.dt * this->problem_.decay_rates[s] / dims
-								 + 2 * this->problem_.dt * get_diffusion_coefficients(x, y, z, s) / (shape * shape);
-					c_bag[idx] = -this->problem_.dt * get_diffusion_coefficients(x, y, z, s) / (shape * shape);
+					auto dim_idx = dim == 'x' ? x : (dim == 'y' ? y : z);
+
+					if (dim_idx == 0)
+					{
+						a_bag[idx] = 0;
+						b_bag[idx] = 1 + this->problem_.dt * this->problem_.decay_rates[s] / dims
+									 + 1 * this->problem_.dt * get_diffusion_coefficients(x, y, z, s) / (shape * shape);
+						c_bag[idx] = -this->problem_.dt * get_diffusion_coefficients(x, y, z, s) / (shape * shape);
+					}
+					else if (dim_idx == n - 1)
+					{
+						a_bag[idx] = -this->problem_.dt * get_diffusion_coefficients(x, y, z, s) / (shape * shape);
+						b_bag[idx] = 1 + this->problem_.dt * this->problem_.decay_rates[s] / dims
+									 + 1 * this->problem_.dt * get_diffusion_coefficients(x, y, z, s) / (shape * shape);
+						c_bag[idx] = 0;
+					}
+					else
+					{
+						a_bag[idx] = -this->problem_.dt * get_diffusion_coefficients(x, y, z, s) / (shape * shape);
+						b_bag[idx] = 1 + this->problem_.dt * this->problem_.decay_rates[s] / dims
+									 + 2 * this->problem_.dt * get_diffusion_coefficients(x, y, z, s) / (shape * shape);
+						c_bag[idx] = -this->problem_.dt * get_diffusion_coefficients(x, y, z, s) / (shape * shape);
+					}
 				}
 }
 
 template <typename real_t>
 void sdd_reference_thomas_solver<real_t>::initialize()
 {
-	precompute_values(a_, b_, c_, this->problem_.dz, this->problem_.dims);
+	precompute_values(ax_, bx_, cx_, this->problem_.dx, this->problem_.nx, this->problem_.dims, 'x');
+	precompute_values(ay_, by_, cy_, this->problem_.dy, this->problem_.ny, this->problem_.dims, 'y');
+	precompute_values(az_, bz_, cz_, this->problem_.dz, this->problem_.nz, this->problem_.dims, 'z');
 	b_scratch_ = std::make_unique<real_t[]>(std::max({ this->problem_.nx, this->problem_.ny, this->problem_.nz })
 											* this->problem_.substrates_count);
 }
@@ -47,9 +69,9 @@ void sdd_reference_thomas_solver<real_t>::solve_x()
 	auto diag_l =
 		noarr::scalar<real_t>() ^ noarr::vectors<'s', 'x'>(this->problem_.substrates_count, this->problem_.nx);
 
-	auto a_bag = noarr::make_bag(dens_l, a_.get());
-	auto b_bag = noarr::make_bag(dens_l, b_.get());
-	auto c_bag = noarr::make_bag(dens_l, c_.get());
+	auto a_bag = noarr::make_bag(dens_l, ax_.get());
+	auto b_bag = noarr::make_bag(dens_l, bx_.get());
+	auto c_bag = noarr::make_bag(dens_l, cx_.get());
 
 	auto d = noarr::make_bag(dens_l, this->substrates_);
 
@@ -72,9 +94,9 @@ void sdd_reference_thomas_solver<real_t>::solve_x()
 					auto idx = noarr::idx<'s', 'x', 'y', 'z'>(s, x, y, z);
 					auto prev_idx = noarr::idx<'s', 'x', 'y', 'z'>(s, x - 1, y, z);
 
-					auto r = a_bag[prev_idx] / scratch[prev_idx];
+					auto r = a_bag[idx] / scratch[prev_idx];
 
-					scratch[idx] = b_bag[idx] - c_bag[idx] * r;
+					scratch[idx] = b_bag[idx] - c_bag[prev_idx] * r;
 
 					d[idx] -= r * d[prev_idx];
 				}
@@ -93,7 +115,7 @@ void sdd_reference_thomas_solver<real_t>::solve_x()
 					auto idx = noarr::idx<'s', 'x', 'y', 'z'>(s, x, y, z);
 					auto next_idx = noarr::idx<'s', 'x', 'y', 'z'>(s, x + 1, y, z);
 
-					d[idx] = (d[idx] - c_bag[next_idx] * d[next_idx]) / scratch[idx];
+					d[idx] = (d[idx] - c_bag[idx] * d[next_idx]) / scratch[idx];
 				}
 			}
 		}
@@ -105,11 +127,11 @@ void sdd_reference_thomas_solver<real_t>::solve_y()
 {
 	auto dens_l = get_substrates_layout();
 	auto diag_l =
-		noarr::scalar<real_t>() ^ noarr::vectors<'s', 'x'>(this->problem_.substrates_count, this->problem_.nx);
+		noarr::scalar<real_t>() ^ noarr::vectors<'s', 'y'>(this->problem_.substrates_count, this->problem_.ny);
 
-	auto a_bag = noarr::make_bag(dens_l, a_.get());
-	auto b_bag = noarr::make_bag(dens_l, b_.get());
-	auto c_bag = noarr::make_bag(dens_l, c_.get());
+	auto a_bag = noarr::make_bag(dens_l, ay_.get());
+	auto b_bag = noarr::make_bag(dens_l, by_.get());
+	auto c_bag = noarr::make_bag(dens_l, cy_.get());
 
 	auto d = noarr::make_bag(dens_l, this->substrates_);
 
@@ -132,9 +154,9 @@ void sdd_reference_thomas_solver<real_t>::solve_y()
 					auto idx = noarr::idx<'s', 'x', 'y', 'z'>(s, x, y, z);
 					auto prev_idx = noarr::idx<'s', 'x', 'y', 'z'>(s, x, y - 1, z);
 
-					auto r = a_bag[prev_idx] / scratch[prev_idx];
+					auto r = a_bag[idx] / scratch[prev_idx];
 
-					scratch[idx] = b_bag[idx] - c_bag[idx] * r;
+					scratch[idx] = b_bag[idx] - c_bag[prev_idx] * r;
 
 					d[idx] -= r * d[prev_idx];
 				}
@@ -153,7 +175,7 @@ void sdd_reference_thomas_solver<real_t>::solve_y()
 					auto idx = noarr::idx<'s', 'x', 'y', 'z'>(s, x, y, z);
 					auto next_idx = noarr::idx<'s', 'x', 'y', 'z'>(s, x, y + 1, z);
 
-					d[idx] = (d[idx] - c_bag[next_idx] * d[next_idx]) / scratch[idx];
+					d[idx] = (d[idx] - c_bag[idx] * d[next_idx]) / scratch[idx];
 				}
 			}
 		}
@@ -165,11 +187,11 @@ void sdd_reference_thomas_solver<real_t>::solve_z()
 {
 	auto dens_l = get_substrates_layout();
 	auto diag_l =
-		noarr::scalar<real_t>() ^ noarr::vectors<'s', 'x'>(this->problem_.substrates_count, this->problem_.nx);
+		noarr::scalar<real_t>() ^ noarr::vectors<'s', 'z'>(this->problem_.substrates_count, this->problem_.nz);
 
-	auto a_bag = noarr::make_bag(dens_l, a_.get());
-	auto b_bag = noarr::make_bag(dens_l, b_.get());
-	auto c_bag = noarr::make_bag(dens_l, c_.get());
+	auto a_bag = noarr::make_bag(dens_l, az_.get());
+	auto b_bag = noarr::make_bag(dens_l, bz_.get());
+	auto c_bag = noarr::make_bag(dens_l, cz_.get());
 
 	auto d = noarr::make_bag(dens_l, this->substrates_);
 
@@ -192,9 +214,9 @@ void sdd_reference_thomas_solver<real_t>::solve_z()
 					auto idx = noarr::idx<'s', 'x', 'y', 'z'>(s, x, y, z);
 					auto prev_idx = noarr::idx<'s', 'x', 'y', 'z'>(s, x, y, z - 1);
 
-					auto r = a_bag[prev_idx] / scratch[prev_idx];
+					auto r = a_bag[idx] / scratch[prev_idx];
 
-					scratch[idx] = b_bag[idx] - c_bag[idx] * r;
+					scratch[idx] = b_bag[idx] - c_bag[prev_idx] * r;
 
 					d[idx] -= r * d[prev_idx];
 				}
@@ -212,7 +234,8 @@ void sdd_reference_thomas_solver<real_t>::solve_z()
 				{
 					auto idx = noarr::idx<'s', 'x', 'y', 'z'>(s, x, y, z);
 					auto next_idx = noarr::idx<'s', 'x', 'y', 'z'>(s, x, y, z + 1);
-					d[idx] = (d[idx] - c_bag[next_idx] * d[next_idx]) / scratch[idx];
+
+					d[idx] = (d[idx] - c_bag[idx] * d[next_idx]) / scratch[idx];
 				}
 			}
 		}
--- src/space-dependent-diffusion/reference_thomas_solver.h
+++ src/space-dependent-diffusion/reference_thomas_solver.h
@@ -12,11 +12,14 @@ class sdd_reference_thomas_solver : public locally_onedimensional_solver,
 
 	using index_t = std::int32_t;
 
-	std::unique_ptr<real_t[]> a_, b_, c_;
+	std::unique_ptr<real_t[]> ax_, bx_, cx_;
+	std::unique_ptr<real_t[]> ay_, by_, cy_;
+	std::unique_ptr<real_t[]> az_, bz_, cz_;
 	std::unique_ptr<real_t[]> b_scratch_;
 
-	void precompute_values(std::unique_ptr<real_t[]>& a, std::unique_ptr<real_t[]>& b, std::unique_ptr<real_t[]>& c,
-						   index_t shape, index_t dims);
+	void precompute_values(std::unique_ptr<real_t[]>& a, std::unique_ptr<real_t[]>& b,
+															std::unique_ptr<real_t[]>& c, index_t shape, index_t n,
+															index_t dims, char dim);
 
 public:
 	auto get_substrates_layout() const { return substrate_layouts::get_sxyz_layout<3>(this->problem_); }
